11c8a29a63641f6599d3e8393e1ccf4cc9993342
Francisco Marcano <fjmn2001@gmail.com>
2021-01-13 17:18:48 -0400

    feat(): implementing save_propuesta v1. Ref SN

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+//        $this->app->bind(
+//            UsuarioRepository::class,
+//            UsuarioEloquentRepository::class
+//        );
     }
 }
--- src/Propuesta/Application/PropuestaCreator.php
+++ src/Propuesta/Application/PropuestaCreator.php
@@ -4,10 +4,28 @@ declare(strict_types=1);
 
 namespace Taller\Propuesta\Application;
 
+use Taller\Propuesta\Domain\Propuesta;
+use Taller\Propuesta\Domain\PropuestaRepository;
+
 final class PropuestaCreator
 {
+    private $repository;
+
+    public function __construct(PropuestaRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
     public function __invoke(PropuestaRequest $request)
     {
-        // TODO: Implement __invoke() method.
+        $propuesta = new Propuesta(
+            $request->nombre(),
+            $request->apellido(),
+            $request->cantidadPrestamo(),
+            $request->debeFactura(),
+            $request->edad(),
+            $request->sueldo()
+        );
+        $this->repository->save($propuesta);
     }
 }
